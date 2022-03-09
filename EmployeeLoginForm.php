1854e27efbe5d4155c0d4df543684369ac990d3d
Kenneth Haight <82239058+Kenneth-Haight@users.noreply.github.com>
2022-03-09 12:06:09 -0500

    Update EmployeeLoginForm.php

--- EmployeeLoginForm.php
+++ EmployeeLoginForm.php
@@ -21,7 +21,7 @@
     $searchemployeeID = addslashes($searchemployeeID);
   }
 
-  $db = new mysqli('localhost', 'haightk1_administrator', 'NYE99xyzCPA', 'haightk1_ProjectDB');
+  $db = new mysqli('localhost', '', '', 'haightk1_ProjectDB');
 
   if (mysqli_connect_errno()) {
      echo 'Error: Could not connect to database.  Please try again later.';
